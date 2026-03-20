a90a6964267267fcec5fb28c780213a0086f9db9
horia08-arch <h.oprescu@student.tue.nl>
2026-03-20 12:08:14 +0100

    fixed logic, made it more modular

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include "keyEvent.h"
 
 std::string convert(int k);
+std::pair <int, int> new_pointer_position(std::pair <int, int> pos, std::string keyStroke, int numberOfLines, std::string words[10001]);
 
 int main() {
     std::string s;
@@ -24,45 +25,15 @@ int main() {
     
     std::pair <int, int> pos = {1, -1};
     std::string keyStroke = "-1";
-    //Looping to detect key strokes until i press ctrl
+    //Looping to detect key strokes until i press ctrl + A
     //TO DO: add enter
-    while(keyStroke != "ctrlandA") {
+    while(true) {
         if(_kbhit()) {
             int k = _getch();
             keyStroke = convert(k);
-
-            //If I can go to the left. If not, I go on a row above
-            if(keyStroke == "arrow_left") {
-                if(pos.second > -1)
-                    pos.second--;
-                else if(pos.second == -1 && pos.first > 1)
-                    pos.first--, pos.second = int(words[pos.first].size() - 1);
-            }
-            //If i can go to the right. If not, I go on a row below
-            else if(keyStroke == "arrow_right") {
-                if(pos.second < int(words[pos.first].size() - 1)) 
-                    pos.second++;
-                else if(pos.second == int(words[pos.first].size() - 1) && pos.first < numberOfLines) {
-                    pos.second = -1, pos.first++;
-                }
-            }
-            //If i can go up. I keep the column or I am at the end of the row if row above is not long enough 
-            else if(keyStroke == "arrow_up") {
-                if(pos.first > 1) {
-                    if(pos.second <= int(words[pos.first - 1].size() - 1))
-                        pos.first--;
-                    else pos.first--, pos.second = int(words[pos.first].size() - 1);
-                }  
-                else pos.first;             
-            }
-            //If i can go down. I keep the column or I am at the end of the row below if it is not long enough
-            else if(keyStroke == "arrow_down") {
-                if(pos.first < numberOfLines) {
-                    if(pos.second <= int(words[pos.first + 1].size() - 1))
-                        pos.first++;
-                    else pos.first++, pos.second = int(words[pos.first].size() - 1);
-                }  
-            }
+            if(keyStroke == "ctrlandA")
+                break;
+            pos = new_pointer_position(pos, keyStroke, numberOfLines, words);
 
             std::cout << pos.first << ' ' << pos.second << '\n'; 
         }
